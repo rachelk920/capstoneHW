3dc9fcc92fab1d69678d955fb25478705c6f7381
Rachel <rmshinski@gmail.com>
2021-06-09 20:37:23 -0400

    homework

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -11,7 +11,24 @@ const Navbar = () => {
           <li><Link to="/home">Home</Link></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
-          <li><a href="/attractions">Attractions</a></li>
+          <li><a href="/attractions">All Attractions</a></li>
+<li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Categories</a>
+    <div class="dropdown-content">
+      <a href="/Breweries">Breweries</a>
+      <a href="/Casinos">Casinos</a>
+      <a href="/Museums">Museums</a>
+      <a href="/Music">Music Venues</a>
+      <a href="/OurFavories">Our Favorites</a>
+      <a href="/Parks">Parks</a>
+      <a href="/Restaurants">Restaurants</a>
+      <a href="/Sports">Sports</a>
+      {/* <a href="#">Visit Downtown</a>
+      <a href="#">Visit Liberty Center</a>
+      <a href="#">Visit Newport</a>
+      <a href="#">Visit OTR</a> */}
+    </div>
+  </li>
         </ul>
       </div>
 
